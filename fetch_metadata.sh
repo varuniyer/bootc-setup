64344cd8c2a86d5cb3ae51aa544fcc8e8e7ffbd3
Varun Iyer <varun@varuniyer.net>
2026-06-07 21:08:02 -0500

    fix typo

--- fetch_metadata.sh
+++ fetch_metadata.sh
@@ -2,4 +2,4 @@
 set -euo pipefail
 
 curl -sSf -H "Metadata-Flavor: Google" \
-    "http://169.254.169.25/computeMetadata/v1/instance/attributes/$1"
+    "http://169.254.169.254/computeMetadata/v1/instance/attributes/$1"
